fda4546b8cc00a7a6a3636312d1f38494944ee39
111yp <lillianpetals@gmail.com>
2020-01-27 20:03:27 -0500

    add content
    added all the information needed within the content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,86 @@
-function detectGem(self) {
-    console.log(self);
-}
+$(document).ready(function() {
+    $('#gem-1').click(function()
+    {
+        $('#content, #gem-2-display, #gem-3-display, #gem-4-display, #gem-5-display, #gem-6-display, #gem-7-display, #gem-8-display, #gem-9-display, #gem-10-display, #gem-11-display, #gem-12-display').fadeOut(0, function()
+        {
+            $('#content, #gem-1-display').fadeIn('slow');
+        });
+    });
+    $('#gem-2').click(function()
+    {
+        $('#content, #gem-1-display, #gem-3-display, #gem-4-display, #gem-5-display, #gem-6-display, #gem-7-display, #gem-8-display, #gem-9-display, #gem-10-display, #gem-11-display, #gem-12-display').fadeOut(0, function()
+        {
+            $('#content, #gem-2-display').fadeIn('slow');
+        });
+    });
+    $('#gem-3').click(function()
+    {
+        $('#content, #gem-2-display, #gem-1-display, #gem-4-display, #gem-5-display, #gem-6-display, #gem-7-display, #gem-8-display, #gem-9-display, #gem-10-display, #gem-11-display, #gem-12-display').fadeOut(0, function()
+        {
+            $('#content, #gem-3-display').fadeIn('slow');
+        });
+    });
+    $('#gem-4').click(function()
+    {
+        $('#content, #gem-2-display, #gem-3-display, #gem-1-display, #gem-5-display, #gem-6-display, #gem-7-display, #gem-8-display, #gem-9-display, #gem-10-display, #gem-11-display, #gem-12-display').fadeOut(0, function()
+        {
+            $('#content, #gem-4-display').fadeIn('slow');
+        });
+    });
+    $('#gem-5').click(function()
+    {
+        $('#content, #gem-2-display, #gem-3-display, #gem-4-display, #gem-1-display, #gem-6-display, #gem-7-display, #gem-8-display, #gem-9-display, #gem-10-display, #gem-11-display, #gem-12-display').fadeOut(0, function()
+        {
+            $('#content, #gem-5-display').fadeIn('slow');
+        });
+    });
+    $('#gem-6').click(function()
+    {
+        $('#content, #gem-2-display, #gem-3-display, #gem-4-display, #gem-5-display, #gem-1-display, #gem-7-display, #gem-8-display, #gem-9-display, #gem-10-display, #gem-11-display, #gem-12-display').fadeOut(0, function()
+        {
+            $('#content, #gem-6-display').fadeIn('slow');
+        });
+    });
+    $('#gem-7').click(function()
+    {
+        $('#content, #gem-2-display, #gem-3-display, #gem-4-display, #gem-5-display, #gem-6-display, #gem-1-display, #gem-8-display, #gem-9-display, #gem-10-display, #gem-11-display, #gem-12-display').fadeOut(0, function()
+        {
+            $('#content, #gem-7-display').fadeIn('slow');
+        });
+    });
+    $('#gem-8').click(function()
+    {
+        $('#content, #gem-2-display, #gem-3-display, #gem-4-display, #gem-5-display, #gem-6-display, #gem-7-display, #gem-1-display, #gem-9-display, #gem-10-display, #gem-11-display, #gem-12-display').fadeOut(0, function()
+        {
+            $('#content, #gem-8-display').fadeIn('slow');
+        });
+    });
+    $('#gem-9').click(function()
+    {
+        $('#content, #gem-2-display, #gem-3-display, #gem-4-display, #gem-5-display, #gem-6-display, #gem-7-display, #gem-8-display, #gem-1-display, #gem-10-display, #gem-11-display, #gem-12-display').fadeOut(0, function()
+        {
+            $('#content, #gem-9-display').fadeIn('slow');
+        });
+    });
+    $('#gem-10').click(function()
+    {
+        $('#content, #gem-2-display, #gem-3-display, #gem-4-display, #gem-5-display, #gem-6-display, #gem-7-display, #gem-8-display, #gem-9-display, #gem-1-display, #gem-11-display, #gem-12-display').fadeOut(0, function()
+        {
+            $('#content, #gem-10-display').fadeIn('slow');
+        });
+    });
+    $('#gem-11').click(function()
+    {
+        $('#content, #gem-2-display, #gem-3-display, #gem-4-display, #gem-5-display, #gem-6-display, #gem-7-display, #gem-8-display, #gem-9-display, #gem-10-display, #gem-1-display, #gem-12-display').fadeOut(0, function()
+        {
+            $('#content, #gem-11-display').fadeIn('slow');
+        });
+    });
+    $('#gem-12').click(function()
+    {
+        $('#content, #gem-2-display, #gem-3-display, #gem-4-display, #gem-5-display, #gem-6-display, #gem-7-display, #gem-8-display, #gem-9-display, #gem-10-display, #gem-11-display, #gem-1-display').fadeOut(0, function()
+        {
+            $('#content, #gem-12-display').fadeIn('slow');
+        });
+    });
+ });
